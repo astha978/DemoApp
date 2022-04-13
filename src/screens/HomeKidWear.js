e0201b9a6b54519d665e7c60534993f8af386ce8
astha978 <chauhanastha11@gmail.com>
2022-04-13 13:09:54 -0700

    this is my first commit

---
+++ src/screens/HomeKidWear.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeKidWear = () => {
+  return (
+    <View>
+      <Text>HomeKidWear</Text>
+    </View>
+  )
+}
+
+export default HomeKidWear
+
+const styles = StyleSheet.create({})
